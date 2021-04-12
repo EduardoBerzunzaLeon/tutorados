478d95da32d1bdf001a8d5430775adf27675188d
unknown <eduardoberzunzal@gmail.com>
2021-04-12 14:34:09 -0500

    change base repository

--- dal/base.repository.js
+++ dal/base.repository.js
@@ -5,7 +5,7 @@ class BaseRepository {
     this.entity = entity;
   }
 
-  async findAll(queryParams = {}, filterFields = {}) {
+  findAll(queryParams = {}, filterFields = {}) {
     const features = new APIFeaturesMongo(
       this.entity.find(filterFields),
       queryParams
@@ -14,40 +14,40 @@ class BaseRepository {
       .sort()
       .limitFields()
       .paginate();
-    return await features.query;
+    return features.query;
   }
 
-  async findById(id, popOptions) {
-    let query = this.this.entity.findById(id);
+  findById(id, popOptions) {
+    let query = this.entity.findById(id);
     if (popOptions) query = query.populate(popOptions);
-    return await query;
+    return query;
   }
 
-  async findOne(searchOptions, popOptions) {
+  findOne(searchOptions, popOptions) {
     let query = this.entity.findOne(searchOptions);
     if (popOptions) query = query.populate(popOptions);
-    return await query;
+    return query;
   }
 
-  async updateById(id, data) {
+  updateById(id, data) {
     delete data?.createdAt;
     delete data?.updatedAt;
-    return await this.entity.findByIdAndUpdate(id, entity, {
+    return this.entity.findByIdAndUpdate(id, data, {
       new: true,
       runValidators: true,
     });
   }
 
-  async deleteById(id) {
-    return await this.entity.findByIdAndDelete(id);
+  deleteById(id) {
+    return this.entity.findByIdAndDelete(id);
   }
 
-  async create(data) {
-    return await this.entity.create([data]);
+  create(data) {
+    return this.entity.create([data]);
   }
 
-  async save(entity) {
-    return await entity.save({ validateBeforeSave: false });
+  save(entity) {
+    return entity.save({ validateBeforeSave: false });
   }
 }
 
